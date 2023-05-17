3b5f3336e26c40b3643de5089783dd50349b09d4
Loading.. Please Wait <69704016+ImLoadingUuU@users.noreply.github.com>
2023-05-17 22:43:35 +0800

    😍💅 Beautiful SQL++

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `clients` (
+CREATE TABLE `da_clients` (
   `username` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE `clients` (
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `websites` (
+CREATE TABLE `da_website` (
   `username` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `suspended` TINYINT NOT NULL,
